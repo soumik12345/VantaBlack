e0699e0c20b177574c1a666cf6c7741f5691e654
soumik12345 <19soumik.rakshit96@gmail.com>
2020-06-16 23:23:41 +0530

    Added Definitions

--- js/definitions.js
+++ js/definitions.js
@@ -1,4 +1,5 @@
-var PIECES = {
+// Mapping of Chess Pieces
+const PIECES = {
     EMPTY: 0,
     WHITE_PAWN: 1,
     WHITE_KNIGHT: 2,
@@ -10,6 +11,73 @@ var PIECES = {
     BLACK_KNIGHT: 8,
     BLACK_BISHOP: 9,
     BLACK_ROOK: 10,
-    BLACK_QUEEN: 12,
-    BLACK_KING: 13
+    BLACK_QUEEN: 11,
+    BLACK_KING: 12
+};
+
+// Length of the Data Structure to represent the board
+const BOARD_SQUARES_NUMBER = 120;
+
+// Mapping of Files (A-H)
+const FILES = {
+    FILE_A: 0,
+    FILE_B: 1,
+    FILE_C: 2,
+    FILE_D: 3,
+    FILE_E: 4,
+    FILE_F: 5,
+    FILE_G: 6,
+    FILE_H: 7,
+    FILE_None: 8,
+};
+
+// Mapping of Ranks (1-8)
+const RANKS = {
+    RANK_1: 0,
+    RANK_2: 1,
+    RANK_3: 2,
+    RANK_4: 3,
+    RANK_5: 4,
+    RANK_6: 5,
+    RANK_7: 6,
+    RANK_8: 7,
+    RANK_None: 8
+}
+
+// Mapping of Colours of Pieces
+const COLORS = {
+    WHITE: 0,
+    BLACK: 1,
+    BOTH: 2
+}
+
+// Mapping of Square Labels
+const SQUARES = {
+    // First 8 squares
+    A1: 21,
+    B1: 22,
+    C1: 23,
+    D1: 24,
+    E1: 25,
+    F1: 26,
+    G1: 27,
+    H1: 28,
+    // Last 8 squares
+    A8: 91,
+    B8: 92,
+    C8: 93,
+    D8: 94,
+    E8: 95,
+    F8: 96,
+    G8: 97,
+    H8: 98,
+    // Exceptions
+    NO_SQUARE: 99,
+    OFF_BOARD: 100
+};
+
+// Mapping of Boolean Literals
+const BOOLEAN = {
+    FALSE: 0,
+    TRUE: 1
 }
